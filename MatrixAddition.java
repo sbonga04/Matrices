13940c599f70124e731317a0c989299b235ca5db
sbonga <4323535@myuwc.ac.za>
2026-05-05 18:48:23 +0200

    read from file as is

--- MatrixAddition.java
+++ MatrixAddition.java
@@ -1,6 +1,22 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class MatrixAddition {
 
-    public static void main() {
-        System.out.println("welcome to matrices");
+    public static void main() throws FileNotFoundException {
+        //System.out.println("welcome to matrices");
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("mat_add.txt"));
+
+            String line;
+            while ((line = reader.readLine()) != null){
+                System.out.println(line);
+            }
+            reader.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
