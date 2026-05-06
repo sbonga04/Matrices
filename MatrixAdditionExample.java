63006e8b198f7c3d726c71888d5b74b0a780a11a
sbonga <4323535@myuwc.ac.za>
2026-05-07 01:06:04 +0200

    added a different approach

---
+++ MatrixAdditionExample.java
@@ -0,0 +1,12 @@
+//In this documet i'm gonna try and make something like the actuall problem that i need to solve at matrixAddtion Main
+//this will make me understand exactly how can i solve the problem better since i won't be reading from a file here
+//i am using my own hard core numbers just to test how the solution might look like
+
+public class MatrixAdditionExample {
+    static void main() {
+        String line = "3 2 5, 4 5 1, 6 3 1; 2 3 1, 3 4 1, 2 1 2";
+
+
+
+    }
+}
