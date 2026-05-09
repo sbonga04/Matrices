9e5cbae38b23c8c427dc8b6a0493357f42ba89b1
sbonga <4323535@myuwc.ac.za>
2026-05-09 14:24:58 +0200

    edited some forLoop

--- MatrixAdditionExample.java
+++ MatrixAdditionExample.java
@@ -3,10 +3,19 @@
 //i am using my own hard core numbers just to test how the solution might look like
 
 public class MatrixAdditionExample {
+
+
     static void main() {
+        //say we got a line of string
         String line = "3 2 5, 4 5 1, 6 3 1; 2 3 1, 3 4 1, 2 1 2";
 
+        //splitting matrix itself now to A and B
+        String[] matrix = line.split(";");
+        //
+        for(int i = 0; i < line.length(); i ++){
+            String[] row = matrix[i].split(",");
 
-
+            int[][] AorB = new int[row.length][];
+        }
     }
 }
