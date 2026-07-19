8050d855c82581a1f5472cf87e35d780b686c529
sbonga <4323535@myuwc.ac.za>
2026-07-19 18:15:09 +0200

    addition of a 1D array successfull

--- MatrixTesting.java
+++ MatrixTesting.java
@@ -1,30 +1,53 @@
-
-
+import java.util.Arrays;
 
 public class MatrixTesting {
 
 	void main() {
 		System.out.println("we're in");
+		int[] arr_A = {1,2,3};
+		int[] arr_B = {1,2,4};
+		System.out.println(Arrays.toString(arrayAddition(arr_A,arr_B)));
 		int[][] A = {{3,4,6},{3,2,2},{1,1,2}};
 		int[][] B = {{1,1,1},{4,3,4},{1,1,1}};
 
-		for (int i = 0; i < A.length ; i++){ // i didn't use i++ but still program increments, insane
-			for (int j = 0; j < A.length;j++ ) {
-				System.out.print("[" + A[i][j] + "]");
-				// if i use break the program will not stop running
-			}
-			System.out.println("");
-		}
-		System.out.print("\nsize of array is :" +A.length); //why wont this display anything on the console but its still the same
-		//line as the one before the nested for loop
-
+//		for (int i = 0; i < A.length ; i++){
+//			for (int j = 0; j < A.length;j++ ) {
+//				System.out.print("[" + A[i][j] + "]");
+//			}
+//			System.out.println("");
+//		}
+		//System.out.print("\nsize of array is :" +A.length);
 	}
 	private void MatrixAddding(int[][] arrayA, int[][] arrayB){
 		if (arrayB.length != arrayA.length)
 			System.err.println("Matrices being must be the same size");
+		else if (arrayB.length == arrayA.length){
 
+		}else
+			System.err.println("invalid inputs");
+
+		if (arrayB.length == arrayA.length){
+			for(int i = 0; i < arrayA.length ; i ++){
+				for (int j = 0; j < arrayA.length ; j++){
+
+				}
+			}
+		}
+	}
+	/*Maybe for what i'm tryna do here, i need to be able to add 2 1D arrays
+  And still be able to produce a new array with the sum of each index in the arrays*/
+
+
+	//A little demo
+	private int[] arrayAddition(int[] arr_A, int[] arr_B){
+		int[] arr_C = new int[arr_B.length];
+		for (int i = 0;i < arr_A.length; i ++){
+			arr_C[i] = arr_A[i] + arr_B[i];
+		}
+		return arr_C;
 	}
 }
+
 
 //for this test i will use built in arrays
 //int[][] A = new int[3][];
