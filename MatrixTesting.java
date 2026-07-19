05a10304711aa4ce363d1661996126df17955a1e
sbonga <4323535@myuwc.ac.za>
2026-07-19 17:48:30 +0200

    testing the array

--- MatrixTesting.java
+++ MatrixTesting.java
@@ -8,6 +8,15 @@ public class MatrixTesting {
 		int[][] A = {{3,4,6},{3,2,2},{1,1,2}};
 		int[][] B = {{1,1,1},{4,3,4},{1,1,1}};
 
+		for (int i = 0; i < A.length ; i++){ // i didn't use i++ but still program increments, insane
+			for (int j = 0; j < A.length;j++ ) {
+				System.out.print("[" + A[i][j] + "]");
+				// if i use break the program will not stop running
+			}
+			System.out.println("");
+		}
+		System.out.print("\nsize of array is :" +A.length); //why wont this display anything on the console but its still the same
+		//line as the one before the nested for loop
 
 	}
 	private void MatrixAddding(int[][] arrayA, int[][] arrayB){
@@ -26,7 +35,6 @@ public class MatrixTesting {
 
 | A | -------> | [0] -> null       |
 +---+          | [1] -> null       |
-
                | [2] -> null       |
                +-------------------+
 */
